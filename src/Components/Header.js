8243ec29935c0aa404bcf24ca7a17f7ffd804840
Thinh Phan <21130548@st.hcmuaf.edu.vn>
2024-06-09 13:52:33 +0700

    header page

---
+++ src/Components/Header.js
@@ -0,0 +1,53 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo  from './assets/images/logo.png';
+import './assets/css/owl.css';
+import './assets/css/animate.css';
+import './assets/css/flex-slider.css';
+import './assets/css/templatemo-cyborg-gaming.css';
+
+export default function header() {
+
+    return (
+
+        <div className="col-12">
+            <header className="header-area header-sticky">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <nav className="main-nav">
+
+                                <a href="index.html" className="logo">
+                                    <img src={logo} alt="Logo"/>
+                                </a>
+
+                                <div className="search-input">
+                                    <form id="search" action="#">
+                                        <input type="text" placeholder="Type Something" id='searchText'
+                                               name="searchKeyword"
+                                               />
+                                        <i className="fa fa-search"></i>
+                                    </form>
+                                </div>
+
+                                <ul className="nav">
+                                    <li><a href="index.html" className="active">Home</a></li>
+                                    <li><a href="browse.html">Browse</a></li>
+                                    <li><a href="details.html">Details</a></li>
+                                    <li><a href="streams.html">Streams</a></li>
+                                    <li><a href="profile.html">Profile <img src="assets/images/profile-header.jpg"
+                                                                            alt=""/></a>
+                                    </li>
+                                </ul>
+                                <a className='menu-trigger'>
+                                    <span>Menu</span>
+                                </a>
+
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+
+    )
+}
---
+++ src/Components/Home.js
@@ -0,0 +1,30 @@
+export default function Home() {
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-content">
+
+
+                        <div class="main-banner">
+                            <div class="row">
+                                <div class="col-lg-7">
+                                    <div class="header-text">
+                                        <h6>Welcome To Cyborg</h6>
+                                        <h4><em>Browse</em> Our Popular Games Here</h4>
+                                        <div class="main-button">
+                                            <a href="browse.html">Browse Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+    )
+}
